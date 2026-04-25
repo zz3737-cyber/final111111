acd41c0e48b52ae202a9e4fd42071cff8eabe486
JL12345555 <102105510+JL12345555@users.noreply.github.com>
2026-04-25 02:47:18 -0400

    pause game and ui

---
+++ Assets/script/GameManager.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using TMPro;
+using System.Collections;
+using UnityEngine.InputSystem;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    [Header("Pickup UI")]
+    public TextMeshProUGUI pickupText;
+
+    [Header("Pause UI")]
+    public GameObject pausePanel;
+
+    private bool isPaused = false;
+
+    private void Awake()
+    {
+        instance = this;
+
+        if (pickupText != null)
+            pickupText.gameObject.SetActive(false);
+
+        if (pausePanel != null)
+            pausePanel.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Gamepad.current != null && Gamepad.current.startButton.wasPressedThisFrame)
+        {
+            TogglePause();
+        }
+    }
+
+    public void ShowText(string message)
+    {
+        StopAllCoroutines();
+        pickupText.text = message;
+        pickupText.gameObject.SetActive(true);
+        StartCoroutine(HideText());
+    }
+
+    private IEnumerator HideText()
+    {
+        yield return new WaitForSeconds(3f);
+        pickupText.gameObject.SetActive(false);
+    }
+
+    public void TogglePause()
+    {
+        isPaused = !isPaused;
+
+        if (pausePanel != null)
+            pausePanel.SetActive(isPaused);
+
+        Time.timeScale = isPaused ? 0f : 1f;
+    }
+
+    public void ResumeGame()
+    {
+        isPaused = false;
+
+        if (pausePanel != null)
+            pausePanel.SetActive(false);
+
+        Time.timeScale = 1f;
+    }
+}
